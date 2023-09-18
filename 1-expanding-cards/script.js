2cc507f42cf77ad69b5add246d08104c8133d53c
Aakash <aakash.bansal@aakashbansal.in>
2023-09-18 09:31:49 +0530

    First day

---
+++ 1-expanding-cards/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) =>
+  card.addEventListener("click", (e) => {
+    cards.forEach((card) => {
+      card.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  })
+);

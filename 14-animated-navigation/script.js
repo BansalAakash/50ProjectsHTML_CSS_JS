e975bf32e1fed2dc896be12529badb727d2cc1c6
Aakash <aakash.bansal@aakashbansal.in>
2023-10-21 09:31:48 +0530

    Completed 15. animated navigation

---
+++ 14-animated-navigation/script.js
@@ -0,0 +1,3 @@
+const menu = document.querySelector(".menu");
+const toggleButton = document.getElementById("toggle");
+toggleButton.addEventListener("click", () => menu.classList.toggle("active"));
